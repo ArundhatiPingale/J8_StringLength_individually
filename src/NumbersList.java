84acdec30e75ee5609541742b42b128f541fcdeb
ARUNDHATI <arundhati@ARUNDHATIs-MacBook-Pro.local>
2022-12-14 20:15:04 +0530

    Changes

--- src/NumbersList.java
+++ src/NumbersList.java
@@ -8,15 +8,13 @@ public class NumbersList {
 		NumbersList(List.of(12, 5, 6, 3,2,4,8,1));
 		
 	}
-	private static boolean isEven(int number)
-	{
-		return number%2==0;
-	}
+
 
 	private static void NumbersList(List<Integer> numbers) {
 		// TODO Auto-generated method stub
 		numbers.stream().
-		filter(NumbersList::isEven)
+		//filter(NumbersList::isEven)
+		filter(number -> number%2==0)
 		.forEach(System.out::println);
 
 	}
