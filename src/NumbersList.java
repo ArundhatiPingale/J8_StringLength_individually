821bd47189795079a51122264853fb83566e1eb1
ARUNDHATI <arundhati@ARUNDHATIs-MacBook-Pro.local>
2022-12-14 20:39:46 +0530

    Changes

--- src/NumbersList.java
+++ src/NumbersList.java
@@ -5,11 +5,9 @@ public class NumbersList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		List<Integer> values=List.of(1,2,3,4,5,6);
-		values.stream().filter(value -> value%2 !=0 ).
-		map(value -> value * value * value).
-		forEach(System.out::println);
-	
+		List<String> courses=List.of("JAVA","SPRING","HIBERNATE","REST","ANGUKAR","WEBSERVICE");
+		
+		courses.stream().map(course -> course.length()).forEach(System.out::println);
 	}
 
 	
