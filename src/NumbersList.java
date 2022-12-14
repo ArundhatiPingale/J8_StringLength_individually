2ddd68dc9ea3b2dfca307ae36926e172390a7875
ARUNDHATI <arundhati@ARUNDHATIs-MacBook-Pro.local>
2022-12-14 20:22:00 +0530

    Changes

--- src/NumbersList.java
+++ src/NumbersList.java
@@ -5,18 +5,11 @@ public class NumbersList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		NumbersList(List.of(12, 5, 6, 3,2,4,8,1));
-		
+		List<String> courses=List.of("JAVA","SPRING","HIBERNATE","REST","ANGUKAR","WEBSERVICE");
+		courses.stream().forEach(System.out::println);
+	
 	}
 
-
-	private static void NumbersList(List<Integer> numbers) {
-		// TODO Auto-generated method stub
-		numbers.stream().
-		//filter(NumbersList::isEven)
-		filter(number -> number%2==0)
-		.forEach(System.out::println);
-
-	}
+	
 
 }
